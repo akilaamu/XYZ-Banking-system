dfaa2cccfe6b6fca856c84f48e81554373dceccd
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-20 14:48:16 +0530

    Update NotificationController.java

--- BankApp/notification-service/src/main/java/com/bank/notification/NotificationController.java
+++ BankApp/notification-service/src/main/java/com/bank/notification/NotificationController.java
@@ -1,1 +1,10 @@
+@RestController
+@RequestMapping("/notify")
+public class NotificationController {
+    @PostMapping
+    public String send(@RequestBody String message) {
+        System.out.println("Notification: " + message);
+        return "Sent";
+    }
+}
 
