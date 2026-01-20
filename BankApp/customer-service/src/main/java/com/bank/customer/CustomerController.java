88c8d3c206ecf20d2b7c778b8793764368563996
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-20 13:36:04 +0530

    Update CustomerController.java

--- BankApp/customer-service/src/main/java/com/bank/customer/CustomerController.java
+++ BankApp/customer-service/src/main/java/com/bank/customer/CustomerController.java
@@ -1,1 +1,20 @@
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    private final CustomerRepository repo;
+
+    public CustomerController(CustomerRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return repo.save(customer);
+    }
+}
 
