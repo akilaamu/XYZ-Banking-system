2465fbe09ebb8d79b785e00b98a8c6a6385dedb1
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-20 13:50:09 +0530

    Update Notification.java

--- BankApp/notification-service/src/main/java/com/bank/notification/Notification.java
+++ BankApp/notification-service/src/main/java/com/bank/notification/Notification.java
@@ -1,1 +1,34 @@
+package com.bank.notification;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
 
