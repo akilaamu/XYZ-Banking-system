cbe96897dda2c686dadf41881b31254c38bff67d
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-20 13:37:12 +0530

    Update LoanController.java

--- BankApp/loan-service/src/main/java/com/bank/loan/LoanController.java
+++ BankApp/loan-service/src/main/java/com/bank/loan/LoanController.java
@@ -1,1 +1,23 @@
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+    private final LoanRepository repo;
+    private final NotificationServiceClient notificationClient;
+
+    public LoanController(LoanRepository repo, NotificationServiceClient notificationClient) {
+        this.repo = repo;
+        this.notificationClient = notificationClient;
+    }
+
+    @PostMapping
+    public Loan applyLoan(@RequestBody Loan loan) {
+        Loan savedLoan = repo.save(loan);
+
+        // Redirect to Notification Service
+        notificationClient.sendNotification(
+            "Loan approved for customer: " + loan.getCustomerId()
+        );
+        return savedLoan;
+    }
+}
 
