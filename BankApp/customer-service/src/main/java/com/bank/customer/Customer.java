89aa0e3d8869db9342e0ea5648f8e954726cc2e2
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-20 13:35:37 +0530

    Update Customer.java

--- BankApp/customer-service/src/main/java/com/bank/customer/Customer.java
+++ BankApp/customer-service/src/main/java/com/bank/customer/Customer.java
@@ -1,1 +1,11 @@
+@Entity
+public class Customer {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+    private double balance;
+
+    // getters & setters
+}
 
