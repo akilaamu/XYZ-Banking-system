156b00bb0d453fe654432022532f55e7d61c6ab1
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-21 22:12:37 +0530

    Update CustomerController.java

--- BankApp/customer-service/src/main/java/com/bank/customer/CustomerController.java
+++ BankApp/customer-service/src/main/java/com/bank/customer/CustomerController.java
@@ -1,20 +1,30 @@
+package com.bank.customer;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-    private final CustomerRepository repo;
 
-    public CustomerController(CustomerRepository repo) {
-        this.repo = repo;
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
     @GetMapping
     public List<Customer> getAllCustomers() {
-        return repo.findAll();
+        return customerRepository.findAll();
     }
 
     @PostMapping
-    public Customer addCustomer(@RequestBody Customer customer) {
-        return repo.save(customer);
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
     }
 }
-
