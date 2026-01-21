792b6de6543be36842071caf0f37e516fdd56156
akilaamu <80111012+akilaamu@users.noreply.github.com>
2026-01-21 22:10:50 +0530

    Update CustomerRepository.java

--- BankApp/customer-service/src/main/java/com/bank/customer/CustomerRepository.java
+++ BankApp/customer-service/src/main/java/com/bank/customer/CustomerRepository.java
@@ -1,7 +1,10 @@
 package com.bank.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
+
 
